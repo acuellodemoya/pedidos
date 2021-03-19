3d15b17f05a33290df1e60e539ef25057e1932a9
Alejandro Cuello <acuellodemoya@gmail.com>
2021-03-19 17:20:19 -0500

    Creados los primero modelos, registrados en admin, se han mostrado algunos datos en el dashboard (9)

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
 # Create your models here.
+
+class Cliente(models.Model):
+    """Modelo de Usuario cliente
+    """
+    nombre = models.CharField(max_length=255, null=True)
+    telefono = models.CharField(max_length=20, null=True)
+    email = models.EmailField(null=True)
+    creado = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Producto(models.Model):
+    """Modelo del producto en la base de datos
+    """
+    CATEGORIA = (
+        ('categoria1', 'categoria1'),
+        ('categoria2', 'categoria2'),
+    )
+
+    nombre = models.CharField(max_length=255, null=True)
+    precio = models.FloatField(null=True)
+    categoria = models.CharField(max_length=255, null=True, choices=CATEGORIA)
+    descripcion = models.TextField(null=True)
+    creado = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.nombre
+
+class Tag(models.Model):
+    nombre = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.nombre
+class Orden(models.Model):
+    """Modelo de orden del cliente a determinado producto
+    """
+    ESTADOS = (
+        ('pendiente', 'pendiente'),
+        ('por salir', 'por salir'),
+        ('enviado', 'enviado'),
+    )
+    cliente = models.ForeignKey(Cliente, null=True, on_delete=models.SET_NULL)
+    producto = models.ForeignKey(Producto, null=True, on_delete=models.SET_NULL)
+    creado = models.DateTimeField(auto_now_add=True, null=True)
+    estado = models.CharField(max_length=255, null=True, choices=ESTADOS)
+    tags = models.ManyToManyField(Tag)
+
+    
+    def __str__(self):
+        return self.estado
+
+
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,12 +1,27 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def inicio(request):
-    return render(request, 'dashboard.html')
+    clientes = Cliente.objects.all()
+    ordenes = Orden.objects.all()
+    total_ordenes = ordenes.count()
+    enviados = ordenes.filter(estado='enviado').count()
+    pendientes = ordenes.filter(estado='pendiente').count()
+
+    context = {
+        'clientes': clientes,
+        'ordenes': ordenes,
+        'total_ordenes': total_ordenes,
+        'enviados': enviados,
+        'pendientes': pendientes
+    }
+    return render(request, 'dashboard.html', context)
 
 def productos(request):
-    return render(request, 'productos.html')
+    productos = Producto.objects.all()
+    return render(request, 'productos.html', {'productos': productos})
 
 def clientes(request):
-    return render(request, 'clientes.html')
+    clientes = Cliente.objects.all()
+    return render(request, 'clientes.html', {'clientes': clientes})
